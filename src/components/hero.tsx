d266a1802d95bdcb37881e5f8628ad68da6629d1
ZyadNotCodingWell <zyaddch@gmail.com>
2024-09-12 15:02:56 +0200

    haha deplayment corrections 	modified:   src/components/hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
         >
           <div className="flex flex-col h-full container self-center mx-auto items-center gap-8 py-28 md:py-48 lg:py-28">
             <div className="flex max-w-5xl text-4xl md:text-6xl lg:text-8xl text-center bg-[linear-gradient(175deg,white_40%,#666666_85%)] text-transparent bg-clip-text font-bold">
-              AI for tomorrow's world is here now
+              AI for tomorrow&apos;s world is here now
             </div>
             <Link href="/dashboard" className="rounded-2xl bg-black/50 hover:bg-black/70 transition duration-500">
               <button className="relative flex flex-none w-fit px-4 group py-3 border border-white/40 border-b-0 backdrop-blur-sm hover:border-white/70 hover:border-b-0 transition duration-500 items-center justify-center rounded-2xl bg-[radial-gradient(80%_90%_at_top_left,#1b69db_50%,#da55af)] text-black/25 hover:text-transparent bg-clip-text text-2xl font-black">
